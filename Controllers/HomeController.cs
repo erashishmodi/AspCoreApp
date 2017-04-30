913060c8a6ade1eaa8b9ef615e48bba51b6997a1
Ashish Modi <i_m_ashish@yahoo.com>
2017-04-30 11:03:19 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.UserName="Ashish Modi";
+            return View();
+        }
+    }
+}
